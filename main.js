546ebb5805777bd1351e6bdcda62e4022d4140f3
mrFillye <58518377+mrFillye@users.noreply.github.com>
2020-09-24 10:49:55 +0300

    fixes some errors

--- main.js
+++ main.js
@@ -1,93 +1,86 @@
-document.getElementById("issueInputForm").addEventListener("submit", saveIssue);
+document.getElementById("issueInputForm").addEventListener("submit", (e) => {
 
-function saveIssue(e) {
+    var issueDesription = document.getElementById("issueDescInput").value;
+    var issueSeverity = document.getElementById("issueSeverityInput").value;
+    var issueAssignedTo = document.getElementById("issueAssignedToInput").value;
+    var issueID = chance.guid();
+    var status = "open";
 
-    const issueDesc = document.getElementById("issueDescInput").value;
-    const issueSeverity = document.getElementById("issueSeverityInput").value;
-    const issueAssignedTo = document.getElementById("issueAssignedToInput").value;
-    const issueId = chance.guid();
-    const issueStatus = "open";
-
-    const issue = {
-        id: issueId,
-        status: issueStatus,
-        desc: issueDesc,
+    var issue = {
+        id: issueID,
+        description: issueDesription,
         severity: issueSeverity,
-        assignedTo: issueAssignedTo
+        assignedTo: issueAssignedTo,
+        status: status
     }
+
 
     if (localStorage.getItem("issues") == null) {
         var issues = [];
         issues.push(issue);
-        localStorage.setItem("issues", JSON.stringify(issues))
+        localStorage.setItem("issues", JSON.stringify(issues));
     } else {
         var issues = JSON.parse(localStorage.getItem("issues"));
         issues.push(issue);
-        localStorage.setItem("issues", JSON.stringify(issues))
+        localStorage.setItem("issues", JSON.stringify(issues));
     }
-
-
-
-
-    document.getElementById("issueInputForm").reset();
-
-    fetchIssues();
 
     e.preventDefault();
-}
 
-function setStatusClosed(id) {
-    var issues = JSON.parse(localStorage.getItem('issues'));
+    fetchIssue();
 
-    for (var i = 0; i < issues.length; i++) {
-        if (issues[i].id == id) {
-            issues[i].status = 'Closed';
-        }
-    }
+})
 
-    localStorage.setItem("issues", JSON.stringify(issues))
-
-    fetchIssues();
-}
-
-
-function deleteIssue(id) {
+const setStatusClosed = (id) => {
     var issues = JSON.parse(localStorage.getItem("issues"));
 
     for (var i = 0; i < issues.length; i++) {
         if (issues[i].id == id) {
-            issues.splice(i, 1);
+            issues[i].status = "closed"
         }
     }
 
-    localStorage.setItem("issues", JSON.stringify(issues))
-
-    fetchIssues();
+    localStorage.setItem("issues", JSON.stringify(issues));
+    fetchIssue();
 }
 
+const deleteIssue = (id) => {
+    var issues = JSON.parse(localStorage.getItem("issues"));
 
-function fetchIssues() {
-    var issues = JSON.parse(localStorage.getItem('issues'));
-    var issuesList = document.getElementById('issuesList');
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues.splice(i, 1)
+        }
+    }
 
-    issuesList.innerHTML = '';
+    localStorage.setItem("issues", JSON.stringify(issues));
+    fetchIssue();
+}
+
+const fetchIssue = () => {
+    var issues = JSON.parse(localStorage.getItem("issues"));
+    var issuesList = document.getElementById("issuesList");
+
+    issuesList.innerHTML = "";
 
     for (var i = 0; i < issues.length; i++) {
         var id = issues[i].id;
-        var desc = issues[i].desc;
+        var description = issues[i].description;
         var severity = issues[i].severity;
-        var assignedTo = issues[i].assignedTo;
+        var assignedTo = issues[i].assignedTo
         var status = issues[i].status;
 
         issuesList.innerHTML +=
             '<div class="well">' +
             '<h6>Issue ID: ' + id + '</h6>' +
             '<p><span class="label label-info">' + status + '</span></p>' +
-            '<h3>' + desc + '</h3>' +
+            '<h3>' + description + '</h3>' +
             '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>' +
             '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>' +
             '<a href="#" onclick="setStatusClosed(\'' + id + '\')" class="btn btn-warning">Close</a> ' +
             '<a href="#" onclick="deleteIssue(\'' + id + '\')" class="btn btn-danger">Delete</a>' +
             '</div>';
     }
+
+
 }
